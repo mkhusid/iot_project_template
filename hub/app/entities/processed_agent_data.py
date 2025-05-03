00701179eb1c0c772febf64cbab4b4534a092c55
Andrii Hrytsenko <andrii.hrytsenko02@gmail.com>
2025-05-03 22:22:16 +0300

    store api adapter main logic was implemented

--- hub/app/entities/processed_agent_data.py
+++ hub/app/entities/processed_agent_data.py
@@ -5,3 +5,21 @@ from app.entities.agent_data import AgentData
 class ProcessedAgentData(BaseModel):
     road_state: str
     agent_data: AgentData
+
+    def serialize (self) -> dict:
+        return {
+            "road_state": self.road_state,
+            "agent_data": {
+                "user_id": 0,
+                "accelerometer": {
+                    "x": self.agent_data.accelerometer.x,
+                    "y": self.agent_data.accelerometer.y,
+                    "z": self.agent_data.accelerometer.z
+                },
+                "gps": {
+                    "latitude": self.agent_data.gps.latitude,
+                    "longitude": self.agent_data.gps.longitude
+                },
+                "timestamp": self.agent_data.timestamp,
+            }
+        }
