a3e34f3a7d4a07301778d81544c3542ce7d81536
mkhusid <max6hc@gmail.com>
2025-05-10 10:34:25 +0300

    [agent] hot fixes

--- agent/src/config.py
+++ agent/src/config.py
@@ -10,9 +10,9 @@ def try_parse(type, value: str):
 
 USER_ID = 1
 # MQTT config
-MQTT_BROKER_HOST = os.environ.get("MQTT_BROKER_HOST") or "mqtt"
+MQTT_BROKER_HOST = os.environ.get("MQTT_BROKER_HOST") or "localhost"
 MQTT_BROKER_PORT = try_parse(int, os.environ.get("MQTT_BROKER_PORT")) or 1883
 MQTT_TOPIC = os.environ.get("MQTT_TOPIC") or "agent"
 
 # Delay for sending data to mqtt in seconds
-DELAY = try_parse(float, os.environ.get("DELAY")) or 1
+DELAY = try_parse(float, os.environ.get("DELAY")) or 2
--- agent/src/main.py
+++ agent/src/main.py
@@ -1,7 +1,9 @@
-from paho.mqtt import client as mqtt_client
-import json
+from datetime import datetime
+import paho.mqtt.client as mqtt
 import time
 from schema.aggregated_data_schema import AggregatedDataSchema
+from schema.gps_schema import GpsSchema
+from schema.accelerometer_schema import AccelerometerSchema
 from file_datasource import FileDatasource
 import config
 
@@ -10,27 +12,42 @@ def connect_mqtt(broker, port):
     """Create MQTT client"""
     print(f"CONNECT TO {broker}:{port}")
 
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(client, userdata, flags, rc, properties=None):
         if rc == 0:
             print(f"Connected to MQTT Broker ({broker}:{port})!")
+            print("[INFO] Client: %s", client)
+            print("[INFO] Userdata: %s", userdata)
+            print("[INFO] Flags: %s", flags)
+            print("[INFO] Properties: %s", properties)
         else:
             print("Failed to connect {broker}:{port}, return code %d\n", rc)
             exit(rc)  # Stop execution
 
-    client = mqtt_client.Client()
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.on_connect = on_connect
     client.connect(broker, port)
     client.loop_start()
     return client
 
 
-def publish(client, topic, datasource, delay):
-    try:
-        datasource.start_reading()
+def publish(client, topic, gps_file, accerelometer_file, delay):
+
+    with open(gps_file, 'r', encoding='utf-8') as gps_file,\
+            open(accerelometer_file, 'r', encoding='utf-8') as accerelometer_file:
         while True:
+            datasource = FileDatasource(accerelometer_file, gps_file)
             time.sleep(delay)
             data = datasource.read()
-            msg = AggregatedDataSchema().dumps(data)
+            print(f"Data: {data}")
+            gps = GpsSchema(latitude=data.gps.latitude, longitude=data.gps.longitude)
+            acc = AccelerometerSchema(
+                x=data.accelerometer.x,
+                y=data.accelerometer.y,
+                z=data.accelerometer.z)
+            msg = AggregatedDataSchema(accelerometer=acc,
+                                       gps=gps,
+                                       timestamp=str(data.timestamp),
+                                       user_id=str(data.user_id)).model_dump_json()
             result = client.publish(topic, msg)
             # result: [0, 1]
             status = result[0]
@@ -39,21 +56,16 @@ def publish(client, topic, datasource, delay):
                 print(f"Send `{msg}` to topic `{topic}`")
             else:
                 print(f"Failed to send message to topic {topic}")
-    except Exception as error:
-        print(f"{error}")
-    finally:
-        datasource.stop_reading()
 
 
 def run():
-    # Prepare mqtt client
+    '''Main function to run the MQTT client and publish data'''
     client = connect_mqtt(config.MQTT_BROKER_HOST, config.MQTT_BROKER_PORT)
-    # Prepare datasource
-    datasource = FileDatasource(
-        "/app/src/data/accelerometer.csv", "/app/src/data/gps.csv")
-    # Infinity publish data
-    publish(client, config.MQTT_TOPIC, datasource, config.DELAY)
+    accerelometer_file = './src/data/accelerometer.csv'
+    gps_file = './src/data/gps.csv'
 
+    # Infinity publish data
+    publish(client, config.MQTT_TOPIC, gps_file, accerelometer_file, delay=config.DELAY)
 
 if __name__ == "__main__":
     run()
